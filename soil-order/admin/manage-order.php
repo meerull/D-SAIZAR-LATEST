a4b083680dff4cc766d4286f75c3546a051ff82e
meerull <2022822454@student.uitm.edu.my>
2025-07-12 21:21:22 +0800

    ALMOST FINAL

--- soil-order/admin/manage-order.php
+++ soil-order/admin/manage-order.php
@@ -1,123 +1,113 @@
 <?php include('partials/menu.php'); ?>
-    <div class="wrapper">
-        <h1>Manage Order</h1>
-    </div>
 
-                <br /><br /><br />
+<div class="wrapper">
+    <h1>Manage Order</h1>
+</div>
 
+<br><br><br>
+
+<?php 
+if(isset($_SESSION['update'])) {
+    echo $_SESSION['update'];
+    unset($_SESSION['update']);
+}
+?>
+<br><br>
+
+<table class="content-table">
+    <tr>
+        <th>S.N.</th>
+        <th>Soil</th>
+        <th>Price</th>
+        <th>Qty</th>
+        <th>Total</th>
+        <th>Order Date</th>
+        <th>Status</th>
+        <th>Customer Name</th>
+        <th>Contact</th>
+        <th>Email</th>
+        <th>Address</th>
+        <th>Delivery Address</th>
+        <th>Payment Proof</th>
+        <th>Update Orders</th>
+    </tr>
+
+    <?php 
+    $sql = "SELECT users.*, tbl_order.*, tbl_payment.payment_prove 
+            FROM users 
+            INNER JOIN tbl_order ON users.id=tbl_order.u_id
+            LEFT JOIN tbl_payment ON tbl_order.payment_id = tbl_payment.payment_id";
+    $res = mysqli_query($conn, $sql);
+    $count = mysqli_num_rows($res);
+
+    $sn = 1;
+
+    if($count>0) {
+        while($row=mysqli_fetch_assoc($res)) {
+            $id = $row['id'];
+            $soil = $row['soil'];
+            $price = $row['price'];
+            $qty = $row['qty'];
+            $total = $row['total'];
+            $order_date = $row['order_date'];
+            $status = $row['status'];
+            $customer_name = $row['customer_name'];
+            $customer_contact = $row['customer_contact'];
+            $customer_email = $row['customer_email'];
+            $customer_address = $row['customer_address'];
+            $delivery_address = $row['delivery_address'];
+            $payment_prove = $row['payment_prove'];
+    ?>
+
+        <tr>
+            <td><?php echo $sn++; ?>.</td>
+            <td><?php echo $soil; ?></td>
+            <td><?php echo $price; ?></td>
+            <td><?php echo $qty; ?></td>
+            <td><?php echo $total; ?></td>
+            <td><?php echo $order_date; ?></td>
+            <td>
                 <?php 
-                    if(isset($_SESSION['update']))
-                    {
-                        echo $_SESSION['update'];
-                        unset($_SESSION['update']);
-                    }
+                if($status=="Ordered") echo "<label>$status</label>";
+                elseif($status=="On Delivery") echo "<label style='color: orange;'>$status</label>";
+                elseif($status=="Delivered") echo "<label style='color: green;'>$status</label>";
+                elseif($status=="Cancelled") echo "<label style='color: red;'>$status</label>";
                 ?>
-                <br><br>
+            </td>
+            <td><?php echo $customer_name; ?></td>
+            <td><?php echo $customer_contact; ?></td>
+            <td><?php echo $customer_email; ?></td>
+            <td><?php echo $customer_address; ?></td>
+            <td><?php echo $delivery_address; ?></td>
+            <td>
+                <?php 
+                if(!empty($payment_prove)) {
+                    $ext = pathinfo($payment_prove, PATHINFO_EXTENSION);
+                    if (in_array($ext, ['jpg','jpeg','png'])) {
+                        echo "<a href='".SITEURL."payment/$payment_prove' target='_blank'><img src='".SITEURL."payment/$payment_prove' width='60px'></a>";
+                    } elseif ($ext == 'pdf') {
+                        echo "<a href='".SITEURL."payment/$payment_prove' target='_blank'>View PDF</a>";
+                    } else {
+                        echo "Unsupported";
+                    }
+                } else {
+                    echo "<label>No Payment</label>";
+                }
+                ?>
+            </td>
+            <td>
+                <a href="<?php echo SITEURL; ?>admin/update-order.php?id=<?php echo $id; ?>">
+                    <img src="../images/icons/update.png"/>
+                </a>
+            </td>
+        </tr>
 
-                <table class="content-table">
-                    <tr>
-                        <th>S.N.</th>
-                        <th>soil</th>
-                        <th>Price</th>
-                        <th>Qty.</th>
-                        <th>Total</th>
-                        <th>Order Date</th>
-                        <th>Status</th>
-                        <th>Customer Name</th>
-                        <th>Contact</th>
-                        <th>Email</th>
-                        <th>Address</th>
-                        <th>Delivery Address</th>
-                        <th>Update Orders</th>
-
-                    </tr>
-
-                    <?php 
-                        //Get all the orders from database
-                        $sql = "SELECT users.*, tbl_order.* FROM users INNER JOIN tbl_order ON users.id=tbl_order.u_id"; // DIsplay the Latest Order at First
-                        //Execute Query
-                        $res = mysqli_query($conn, $sql);
-                        //Count the Rows
-                        $count = mysqli_num_rows($res);
-
-                        $sn = 1; //Create a Serial Number and set its initail value as 1
-
-                        if($count>0)
-                        {
-                            //Order Available
-                            while($row=mysqli_fetch_assoc($res))
-                            {
-                                //Get all the order details
-                                $id = $row['id'];
-                                $soil = $row['soil'];
-                                $price = $row['price'];
-                                $qty = $row['qty'];
-                                $total = $row['total'];
-                                $order_date = $row['order_date'];
-                                $status = $row['status'];
-                                $customer_name = $row['customer_name'];
-                                $customer_contact = $row['customer_contact'];
-                                $customer_email = $row['customer_email'];
-                                $customer_address = $row['customer_address'];
-                                
-                                ?>
-
-                                    <tr>
-                                        <td><?php echo $sn++; ?>. </td>
-                                        <td><?php echo $soil; ?></td>
-                                        <td><?php echo $price; ?></td>
-                                        <td><?php echo $qty; ?></td>
-                                        <td><?php echo $total; ?></td>
-                                        <td><?php echo $order_date; ?></td>
-
-                                        <td>
-                                            <?php 
-                                                // Ordered, On Delivery, Delivered, Cancelled
-
-                                                if($status=="Ordered")
-                                                {
-                                                    echo "<label>$status</label>";
-                                                }
-                                                elseif($status=="On Delivery")
-                                                {
-                                                    echo "<label style='color: orange;'>$status</label>";
-                                                }
-                                                elseif($status=="Delivered")
-                                                {
-                                                    echo "<label style='color: green;'>$status</label>";
-                                                }
-                                                elseif($status=="Cancelled")
-                                                {
-                                                    echo "<label style='color: red;'>$status</label>";
-                                                }
-                                            ?>
-                                        </td>
-
-                                        <td><?php echo $customer_name; ?></td>
-                                        <td><?php echo $customer_contact; ?></td>
-                                        <td><?php echo $customer_email; ?></td>
-                                        <td><?php echo $customer_address; ?></td>
-                                        <td><?php echo $row['delivery_address']; ?></td>
-                                        <td>
-                                            <a href="<?php echo SITEURL; ?>admin/update-order.php?id=<?php echo $id; ?>"><img src="../images/icons/update.png"/></a>
-                                        </td>
-                                    </tr>
-
-                                <?php
-
-                            }
-                        }
-                        else
-                        {
-                            //Order not Available
-                            echo "<tr><td colspan='12' class='error'>Orders not Available</td></tr>";
-                        }
-                    ?>
-
- 
-                </table>
- 
-    
-
+    <?php
+        }
+    } else {
+        echo "<tr><td colspan='14' class='error'>Orders not Available</td></tr>";
+    }
+    ?>
+</table>
 
 <?php include('partials/footer.php'); ?>
--- soil-order/order.php
+++ soil-order/order.php
@@ -1,72 +1,41 @@
-
 <?php include('partials-front/menu.php'); ?>
 
 <?php 
-    //CHeck whether soil id is set or not
-    if(isset($_GET['soil_id']))
-    {
-        //Get the soil id and details of the selected soil
-        $soil_id = $_GET['soil_id'];
+if(isset($_GET['soil_id'])) {
+    $soil_id = $_GET['soil_id'];
+    $sql = "SELECT * FROM tbl_soil WHERE id=$soil_id";
+    $res = mysqli_query($conn, $sql);
+    $count = mysqli_num_rows($res);
 
-        //Get the DEtails of the SElected soil
-        $sql = "SELECT * FROM tbl_soil WHERE id=$soil_id";
-        //Execute the Query
-        $res = mysqli_query($conn, $sql);
-        //Count the rows
-        $count = mysqli_num_rows($res);
-        //CHeck whether the data is available or not
-        if($count==1)
-        {
-            //WE Have DAta
-            //GEt the Data from Database
-            $row = mysqli_fetch_assoc($res);
-
-            $title = $row['title'];
-            $price = $row['price'];
-            $image_name = $row['image_name'];
-        }
-        else
-        {
-            //soil not Availabe
-            //REdirect to Home Page
-            header('location:'.SITEURL);
-        }
-    }
-    else
-    {
-        //Redirect to homepage
+    if($count==1) {
+        $row = mysqli_fetch_assoc($res);
+        $title = $row['title'];
+        $price = $row['price'];
+        $image_name = $row['image_name'];
+    } else {
         header('location:'.SITEURL);
     }
+} else {
+    header('location:'.SITEURL);
+}
 ?>
 
-<!-- soil sEARCH Section Starts Here -->
 <section class="soil-order">
-    <div class="container">        
+    <div class="container">
         <h2 class="text-center text-white">Please confirm to place order</h2>
 
-        <form action="" method="POST" class="order">
+        <form action="" method="POST" class="order" enctype="multipart/form-data">
             <fieldset>
                 <legend style="color:white;">Selected Soil</legend>
 
                 <div class="soil-menu-img">
                     <?php 
-                    
-                        //CHeck whether the image is available or not
-                        if($image_name=="")
-                        {
-                            //Image not Availabe
-                            echo "<div class='error'>Image not Available.</div>";
-                        }
-                        else
-                        {
-                            //Image is Available
-                            ?>
-                            <img src="<?php echo SITEURL; ?>images/soil/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
-                            <?php
-                        }
-                    
+                    if($image_name=="") {
+                        echo "<div class='error'>Image not Available.</div>";
+                    } else {
+                        echo "<img src='".SITEURL."images/soil/".$image_name."' class='img-responsive img-curve'>";
+                    }
                     ?>
-                    
                 </div>
 
                 <div class="soil-menu-desc">
@@ -78,90 +47,85 @@
 
                     <div class="order-label" style="color:white;">Quantity</div>
                     <input type="number" name="qty" class="input-responsive" value="1" required>
-                    
                 </div>
-
             </fieldset>
-            
+
             <fieldset>
                 <div class="order-label" style="color:white;">Delivery Address</div>
                 <textarea name="delivery_address" class="input-responsive" required></textarea>
+
+                <div class="order-label" style="color:white;">Upload Payment Proof (image/pdf)</div>
+                <input type="file" name="payment_proof" class="form-control" accept=".jpg,.jpeg,.png,.pdf" required>
+
+                <br>
+                <input type="submit" name="submit_payment" value="Confirm Order & Upload Payment" class="btn btn-primary">
             </fieldset>
-
-            <fieldset>
-                <input type="submit" name="submit" value="Confirm Order" class="btn btn-primary">
-            </fieldset>
-
-
         </form>
 
-        <?php 
+        <?php
+        if (isset($_POST['submit_payment'])) {
+            if(empty($_SESSION["u_id"])) {
+                header('location:login.php');
+            } else {
+                $soil = $_POST['soil'];
+                $price = $_POST['price'];
+                $qty = $_POST['qty'];
+                $total = $price * $qty;
+                date_default_timezone_set('Asia/Kuala_Lumpur');
+                $order_date = date("Y-m-d H:i:s");
+                $status = "Ordered";
+                $u_id = $_SESSION["u_id"];
+                $delivery_address = mysqli_real_escape_string($conn, $_POST['delivery_address']);
 
-            //CHeck whether submit button is clicked or not
-            if(isset($_POST['submit']))
-            {
-                if(empty($_SESSION["u_id"]))
-{
-header('location:login.php');
-}
-else{
-  // Get all the details from the form
+                // Payment upload
+                $payment_date = date("Y-m-d");
+                $payment_time = date("H:i:s");
+                $filename = $_FILES["payment_proof"]["name"];
+                $tempfile = $_FILES["payment_proof"]["tmp_name"];
+                $folder = "payment/" . $filename;
 
-  $soil = $_POST['soil'];
-  $price = $_POST['price'];
-  $qty = $_POST['qty'];
+                if ($filename == "") {
+                    echo "<div class='alert alert-danger'>No file selected.</div>";
+                } else {
+                    move_uploaded_file($tempfile, $folder);
+                    $sql_payment = "INSERT INTO tbl_payment (payment_date, payment_time, payment_prove) 
+                                    VALUES ('$payment_date', '$payment_time', '$filename')";
+                    $res_payment = mysqli_query($conn, $sql_payment);
 
-  $total = $price * $qty; // total = price x qty 
+                    if ($res_payment) {
+                        $payment_id = mysqli_insert_id($conn);
 
-date_default_timezone_set('Asia/Kuala_Lumpur'); 
+                        $sql_order = "INSERT INTO tbl_order SET 
+                            soil = '$soil',
+                            price = $price,
+                            qty = $qty,
+                            total = $total,
+                            order_date = '$order_date',
+                            status = '$status',
+                            u_id='$u_id',
+                            delivery_address = '$delivery_address',
+                            payment_id = '$payment_id'";
 
-  $order_date = date("Y-m-d H:i:s"); //Order DAte
+                        $res_order = mysqli_query($conn, $sql_order);
 
-  $status = "Ordered";  // Ordered, On Delivery, Delivered, Cancelled
-  $u_id=$_SESSION["u_id"];
-  
-  $delivery_address = mysqli_real_escape_string($conn, $_POST['delivery_address']);
-
-
-
-  //Save the Order in Databaase
-  //Create SQL to save the data
-  $sql2 = "INSERT INTO tbl_order SET 
-      soil = '$soil',
-      price = $price,
-      qty = $qty,
-      total = $total,
-      order_date = '$order_date',
-      status = '$status',
-      u_id='$u_id',
-      delivery_address = '$delivery_address'
-       ";
-
-  //echo $sql2; die();
-
-  //Execute the Query
-  $res2 = mysqli_query($conn, $sql2);
-
-  //Check whether query executed successfully or not
-  if($res2==true)
-  {
-      //Query Executed and Order Saved
-      
-      $_SESSION['order'] = "<div class='success text-center'>soil Ordered Successfully.</div>";
-      header('location:'.SITEURL);
-  }
-  else
-  {
-      //Failed to Save Order
-      $_SESSION['order'] = "<div class='error text-center'>Failed to Order soil.</div>";
-      header('location:'.SITEURL);
-  }
-}
+                        if ($res_order) {
+                            $_SESSION['order'] = "<div class='success text-center'>Order placed and payment uploaded successfully.</div>";
+                            header('location:'.SITEURL);
+                        } else {
+                            $_SESSION['order'] = "<div class='error text-center'>Order failed.</div>";
+                            header('location:'.SITEURL);
+                        }
+                    } else {
+                        $_SESSION['order'] = "<div class='error text-center'>Payment upload failed.</div>";
+                        header('location:'.SITEURL);
+                    }
+                }
             }
+        }
         ?>
-
     </div>
 </section>
-<!-- soil sEARCH Section Ends Here -->
 
 <?php include('partials-front/footer.php'); ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
