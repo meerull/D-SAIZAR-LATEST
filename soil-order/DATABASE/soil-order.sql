42e51d9a4a4542b324bbe911908497a9421d120c
meerull <2022822454@student.uitm.edu.my>
2025-07-31 12:20:41 +0800

    submit

--- soil-order/DATABASE/soil-order.sql
+++ soil-order/DATABASE/soil-order.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 26, 2025 at 03:08 AM
+-- Generation Time: Jul 31, 2025 at 04:19 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -121,7 +121,13 @@ INSERT INTO `tbl_order` (`id`, `soil`, `price`, `qty`, `total`, `order_date`, `s
 (60, 'Top Soil', 40.00, 1, 40.00, '2025-07-26 02:22:41', 'On Delivery', 'Taman Perlis Jaya, Perlis', 0, 68),
 (61, 'Red Soil', 30.00, 2, 60.00, '2025-07-26 02:22:41', 'Ordered', 'Jalan Pauh, Perlis', 0, 69),
 (62, 'Red Soil', 30.00, 1, 30.00, '2025-07-26 10:32:31', 'Delivered', 'Kampung Padang Nyu, 02600 Arau, Perlis', 6, 70),
-(63, 'Top Soil', 40.00, 2, 80.00, '2025-07-26 10:47:04', 'Delivered', 'Kg Arau, 16010 Kangar, Perlis', 7, 71);
+(63, 'Top Soil', 40.00, 2, 80.00, '2025-07-26 10:47:04', 'Delivered', 'Kg Arau, 16010 Kangar, Perlis', 7, 71),
+(64, 'Big Rock', 30.00, 15, 450.00, '2025-07-27 08:57:27', 'Delivered', 'taman alia', 8, 72),
+(65, 'Fine sand', 30.00, 5, 150.00, '2025-07-27 21:57:38', 'Delivered', 'UiTM Machang', 9, 73),
+(66, 'Red Soil', 30.00, 1, 30.00, '2025-07-27 22:02:59', 'Delivered', 'Kampung Padang Nyu,Perlis', 10, 70),
+(67, 'Small Rock', 45.00, 2, 90.00, '2025-07-27 22:05:34', 'Cancelled', 'hey', 11, 74),
+(68, 'Fine sand', 30.00, 16, 480.00, '2025-07-27 22:09:46', 'Ordered', 'sms', 12, 75),
+(69, 'Clay sand', 35.00, 2, 70.00, '2025-07-28 13:37:08', 'Delivered', 'Lot 1, Kampung Pauh, Arau, Perlis', 13, 78);
 
 -- --------------------------------------------------------
 
@@ -142,7 +148,13 @@ CREATE TABLE `tbl_payment` (
 
 INSERT INTO `tbl_payment` (`payment_id`, `payment_date`, `payment_time`, `payment_prove`) VALUES
 (6, '2025-07-26', '10:32:31', 0x526563656970742d42616e6b2d52616b7961745f32303235303732363032333134393738362e706466),
-(7, '2025-07-26', '10:47:04', 0x576861747341707020496d61676520323032352d30372d32362061742031302e34362e31325f34656635626566662e6a7067);
+(7, '2025-07-26', '10:47:04', 0x576861747341707020496d61676520323032352d30372d32362061742031302e34362e31325f34656635626566662e6a7067),
+(8, '2025-07-27', '08:57:27', 0x526563656970742d42616e6b2d52616b7961745f32303235303732363032333134393738362e706466),
+(9, '2025-07-27', '21:57:38', 0x526563656970742d42616e6b2d52616b7961745f32303235303732363032333134393738362e706466),
+(10, '2025-07-27', '22:02:59', 0x576861747341707020496d61676520323032352d30372d32362061742031302e34362e31325f34656635626566662e6a7067),
+(11, '2025-07-27', '22:05:34', 0x576861747341707020496d61676520323032352d30372d32362061742031302e34362e31325f34656635626566662e6a7067),
+(12, '2025-07-27', '22:09:46', 0x526563656970742d42616e6b2d52616b7961745f32303235303732363032333134393738362e706466),
+(13, '2025-07-28', '13:37:08', 0x526563656970742d42616e6b2d52616b7961745f32303235303732363032333134393738362e706466);
 
 -- --------------------------------------------------------
 
@@ -166,13 +178,14 @@ CREATE TABLE `tbl_soil` (
 --
 
 INSERT INTO `tbl_soil` (`id`, `title`, `description`, `price`, `image_name`, `category_id`, `featured`, `active`) VALUES
-(35, 'Fine sand', '', 30.00, 'soil-Name-4902.jpg', 5, 'Yes', 'Yes'),
+(35, 'Fine sand', 'Suitable for Panda/Lion Cement', 30.00, 'soil-Name-4902.jpg', 5, 'Yes', 'Yes'),
 (36, 'Red Soil', 'Good for tree', 30.00, 'soil-Name-1139.jpg', 4, 'Yes', 'Yes'),
 (37, 'Small Rock', ' ', 45.00, 'soil-Name-234.jpeg', 9, 'Yes', 'Yes'),
 (39, 'Big Rock', '', 30.00, 'soil-Name-1867.jpg', 9, 'No', 'Yes'),
 (40, 'Limestone', '', 50.00, 'soil-Name-3992.jpg', 9, 'No', 'Yes'),
-(41, 'clay sand', '', 35.00, 'soil-Name-2216.jpg', 5, 'No', 'Yes'),
-(42, 'Top Soil', '.', 40.00, 'soil-Name-6421.jpg', 4, 'Yes', 'Yes');
+(41, 'Clay sand', '', 35.00, 'soil-Name-2216.jpg', 5, 'No', 'Yes'),
+(42, 'Top Soil', '.', 40.00, 'soil-Name-6421.jpg', 4, 'Yes', 'Yes'),
+(43, 'Medium Rock', 'good for base', 60.00, 'soil-Name-6262.webp', 9, 'No', 'Yes');
 
 -- --------------------------------------------------------
 
@@ -227,7 +240,13 @@ INSERT INTO `users` (`id`, `username`, `password`, `customer_name`, `customer_em
 (68, 'nazmi', '$2y$10$hash', 'Nazmi bin Syahmi', 'nazmi@example.com', 601234567029, 'Taman Perlis Jaya, Perlis', '2025-07-26 02:22:41'),
 (69, 'saufi', '$2y$10$hash', 'Saufi bin Nordin', 'saufi@example.com', 601234567030, 'Jalan Pauh, Perlis', '2025-07-26 02:22:41'),
 (70, 'amirul', '$2y$10$glZIukC6ku6k/lFIWI/4teOTtxTx0UW1aiPWcSx09r0f968Xut68G', 'Muhammad Amirul Amin Bin Rosehadi', 'amin@gmail.com', 60174725254, 'Kampung Padang Telela, Arau 02600 Perlis.', '2025-07-26 10:30:23'),
-(71, 'rais', '$2y$10$15IGJS5ckyHLhd4R2y2so.nBqSRe313EVokfnUXo6m2ILBpDV2kOu', 'Muhammad Rais Bin Rosdi', 'rais@gmail.com', 60138966602, 'Kg. Mulong, 16010 Kota Bharu, Kelantan         ', '2025-07-26 10:37:28');
+(71, 'rais', '$2y$10$15IGJS5ckyHLhd4R2y2so.nBqSRe313EVokfnUXo6m2ILBpDV2kOu', 'Muhammad Rais Bin Rosdi', 'rais@gmail.com', 60138966602, 'Kg. Mulong, 16010 Kota Bharu, Kelantan         ', '2025-07-26 10:37:28'),
+(72, 'pit', '$2y$10$16GLQmUMiNmIcZzeJmqY6.pEubW7Uol1BWZ03vXdLefE9Vqfq1gfq', 'shafiq', 'pit@gmail.com', 108447545, 'lot 4049', '2025-07-27 08:56:18'),
+(73, 'hanis tumidi', '$2y$10$lddDSTpEaGcmaKiEBNt./.sSO0Ev8Li86uKMHHXUArirDAwgeoBwi', 'HANIS BINTI ABDUL TUMIDI', 'hanistumidi@gmail.com', 601127262470, '      UiTM Machang Kelantan   ', '2025-07-27 21:56:19'),
+(75, 'hey', '$2y$10$SCbtND3R7hw8rRdnoRjm7Ooo9YdkkTt3vYCkxEFFj2aRP.7jrDyZe', 'adham', 'hey@hey', 601117927399, 'hey         ', '2025-07-27 22:09:19'),
+(76, 'rayyan', '$2y$10$uQ2MjbZzgBc4af8.cpAu9uASLJuO6HyUkLwVNsfoaSexEZvKx7Su.', 'Ahmad Rayyan', 'rayyan@gmail.com', 60174725254, 'Jalan Kampung Nyu, Arau, Perlis', '2025-07-28 12:14:59'),
+(77, 'irfan', '$2y$10$9F40GjZo70jUM.L2XM7Tne5bk.wfbTu2OaWFvw8XP1Z/HB8hssKO2', 'irfan hafizar', 'irfanhafizar@gmail.com', 60184016804, '210A kampung sri perigi, arau perlis', '2025-07-28 13:30:44'),
+(78, 'Adham', '$2y$10$Rl4iNKM23VPUxryO0Hirxe0SZVrweYTMCR7sTAvALqwX.26kzrAie', 'Ahmad Adham', 'adham@gmail.com', 60174725254, '         Lot 1, kampung pauh, arau, perlis', '2025-07-28 13:35:25');
 
 --
 -- Indexes for dumped tables
@@ -284,31 +303,31 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT for table `tbl_category`
 --
 ALTER TABLE `tbl_category`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tbl_order`
 --
 ALTER TABLE `tbl_order`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT for table `tbl_payment`
 --
 ALTER TABLE `tbl_payment`
-  MODIFY `payment_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `payment_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tbl_soil`
 --
 ALTER TABLE `tbl_soil`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
